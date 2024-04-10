5a64310cf8b5773eceb4241e84a9a3ca8eb8b3d7
Abiodun Lasisi <lasisiabioduntowobola@gmail.com>
2024-04-10 19:27:13 +0100

    create vpc

---
+++ project-zenith-example/main.tf
@@ -0,0 +1,25 @@
+/**************************************
+*       configure aws provider        *
+***************************************/
+provider "aws" {
+  region = var.region
+}
+
+
+/**************************
+*       create vpc        *
+***************************/
+
+module "vpc" {
+  source                             = "../module/vpc"
+  region                             = var.region
+  vpc_cidr_block                     = var.vpc_cidr_block
+  project_name                       = var.project_name
+  public_subnet_az1_cidr_block       = var.public_subnet_az1_cidr_block
+  public_subnet_az2_cidr_block       = var.public_subnet_az2_cidr_block
+  private_app_subnet_az1_cidr_block  = var.private_app_subnet_az1_cidr_block
+  private_app_subnet_az2_cidr_block  = var.private_app_subnet_az2_cidr_block
+  private_data_subnet_az1_cidr_block = var.private_data_subnet_az1_cidr_block
+  private_data_subnet_az2_cidr_block = var.private_data_subnet_az2_cidr_block
+
+}
