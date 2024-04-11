4739ad8f7fdc19f84f5e98a3bc372e80b97885b7
Abiodun Lasisi <lasisiabioduntowobola@gmail.com>
2024-04-11 11:34:51 +0100

    update README

--- module/vpc/variables.tf
+++ module/vpc/variables.tf
@@ -1,43 +1,44 @@
 variable "region" {
   description = "region to deploy resources in"
-  type = string
+  type        = string
 }
 
 variable "project_name" {
   description = "project name"
-  type = string
+  type        = string
 }
 
 variable "vpc_cidr_block" {
   description = "vpc cidr block"
-  type = string
+  type        = string
 }
 
 variable "public_subnet_az1_cidr_block" {
   description = "public subnet az1 cidr block"
-  type = string
+  type        = string
 }
 
 variable "public_subnet_az2_cidr_block" {
   description = "public subnet az2 cidr block"
-  type = string
+  type        = string
 }
 
 variable "private_app_subnet_az1_cidr_block" {
   description = "private app subnet az1"
-  type = string
+  type        = string
 }
 
 variable "private_app_subnet_az2_cidr_block" {
   description = "private app subnet az2 cidr block"
+  type        = string
 }
 
 variable "private_data_subnet_az1_cidr_block" {
   description = "private data subnet az1 cidr block"
-  type = string
+  type        = string
 }
 
 variable "private_data_subnet_az2_cidr_block" {
   description = "private data subnet az2 cidr block"
-  type = string
+  type        = string
 }
